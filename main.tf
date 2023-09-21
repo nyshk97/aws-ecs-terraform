71750d0f7e2908117c23c41c4032a75753abb2e8
tsubasa namatame <nyshk97@gmail.com>
2023-09-21 16:35:18 +0900

    Create network

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ terraform {
     encrypt        = true
   }
 }
+
+required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.0"
+   }
+ }
+}
+
+ provider "aws" {
+   region = local.region
+ }
